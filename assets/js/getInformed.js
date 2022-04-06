b8af62f63a9f0bd404df7048228a31647a7d2f87
colin brindle <hcolinbrindle@gmail.com>
2022-04-06 11:30:21 -0400

    Saving changes to getInformed.html and getInformed.js before pulling down and merging

--- assets/js/getInformed.js
+++ assets/js/getInformed.js
@@ -4,30 +4,41 @@ var currentDate = moment().format("YYYY-MM-DD");
 
 var apiKey = "5dff9d1b5af740dab33ec03db22e27f7";
 
-var newsURL = "https://newsapi.org/v2/top-headlines?q=ukraine&from="+currentDate+"&to=&"+currentDate+"language=en&apiKey=";
+var todayNewsAPI = "https://newsapi.org/v2/top-headlines?q=ukraine&from="+currentDate+"&to=&"+currentDate+"&language=en&apiKey=";
 
 var todaySection = $("#today-section");
 
-
 console.log(currentDate);
 
-console.log(newsURL+apiKey);
+console.log(todayNewsAPI+apiKey);
 
-$.ajax({
-    url: newsURL+apiKey,
+var emptyArray = [];
+// For loop, for every object in the JSON response, add to the array first, then create a card then append that card to the page
+
+function getNewsToday(){
+  $.ajax({
+    url: todayNewsAPI+apiKey,
     method: "GET",
-}).then(function (response) { 
-    //Within this function, we will need to write the code to populate cards in the getInformed.html file
-    console.log(response);
-    for (var i = 0; i < 8; i++) {
-        var articleTitle = response.articles[i].title;
-        var articleDesc = response.articles[i].description;
-        var articleURL = response.articles[i].url;
-        console.log(articleTitle, "\n", articleDesc, "\n", articleURL);
-    }
-});
+  }).then(function (response) { 
+      //Within this function, we will need to write the code to populate cards in the getInformed.html file
+      console.log(response);
+      for (var i = 0; i < 8; i++) {
+          var articleTitle = response.articles[i].title;
+          var articleDesc = response.articles[i].description;
+          var articleURL = response.articles[i].url;
+          console.log(articleTitle, "\n", articleDesc, "\n", articleURL);
 
-todaySection.append("Test");
+          var card = $("<div>").addClass("card mb-3");
+          var cardImg = $("<img>").addClass("card-img-top");
+          var cardBody = $("<div>").addClass("card-body");
+          var cardTitle = $("<h5>").addClass("card-title");
+          var cardText = $("<p>").addClass("card-text");
+
+      }
+  });
+}
+
+// todaySection.append("Test");
 
 var userName = document.getElementById("userName");
 var userEmail = document.getElementById("userEmail");

477af9a153e565f46ac121176747658ec8fcfff4
Brandon Snyder <brandon.snyder019@gmail.com>
2022-04-07 10:53:05 -0400

    recommit

--- assets/js/getInformed.js
+++ assets/js/getInformed.js
@@ -1,18 +1,4 @@
-                                               ,w.
-                                             ,YWMMw  ,M  ,
-                        _.---.._   __..---._.'MMMMMw,wMWmW,
-                   _.-""        """           YP"WMMMMMMMMMb,
-                .-' __.'                   .'     MMMMW^WMMMM;
-    _,        .'.-'"; `,       /`     .--""      :MMM[==MWMW^;
- ,mM^"     ,-'.'   /   ;      ;      /   ,       MMMMb_wMW"  @\
-,MM:.    .'.-'   .'     ;     `\    ;     `,     MMMMMMMW `"=./`-,
-WMMm__,-'.'     /      _.\      F"""-+,,   ;_,_.dMMMMMMMM[,_ / `=_}
-"^MP__.-'    ,-' _.--""   `-,   ;       \  ; ;MMMMMMMMMMW^``; __|
-           /   .'            ; ;         )  )`{  \ `"^W^`,   \  :
-          /  .'             /  (       .'  /     Ww._     `.  `"
-         /  Y,              `,  `-,=,_{   ;      MMMP`""-,  `-._.-,
-fsc     (--, )                `,_ / `) \/"")      ^"      `-, -;"\:
-         `"""                    `"""   `"'                  `---"
+
 
 
 console.log("beep boop link established 🤖️");
